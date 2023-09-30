8a416723d0a263c02d76fae1ec1c78f87d3da11c
joko1931 <okoonja@mail.uc.edu>
2023-09-30 11:37:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double celsiusNumber = 0;
+        double fahrenheitNumber = 0;
+
+        System.out.println("Please enter degrees in C: ");
+
+        if (in.hasNextDouble())
+        {
+            celsiusNumber = in.nextDouble();
+            fahrenheitNumber = celsiusNumber * 1.8 + 32;
+            System.out.println(celsiusNumber + " degrees C is " + fahrenheitNumber + " degrees Fah");
+
+        }
+    }
+}
